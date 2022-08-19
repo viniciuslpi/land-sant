2d61742ba6e04e120f8c4b50b0bb21875bb70a73
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-08-19 17:12:55 -0300

    new styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'land-sant';
-  imageObject = [{
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-    title: 'Hummingbirds are amazing creatures'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-    title: 'Example with title.'
-},{
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-    title: 'Hummingbirds are amazing creatures'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg'
-}, {
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-    title: 'Example two with title.'
-}];
+  img_slide = "https://tems.com.br/wp-content/uploads/2022/08/MicrosoftTeams-image-3.jpg";
+
+  changeSlide(image: Number) {
+    if(image === 1) {
+        this.img_slide = "https://tems.com.br/wp-content/uploads/2022/08/MicrosoftTeams-image-3.jpg";
+    }
+    if(image === 2) {
+        this.img_slide = "https://tems.com.br/wp-content/uploads/2022/08/encontro-cla╠a-18-.ml-7875.jpg";
+    }
+    if(image === 3) {
+        this.img_slide = "https://tems.com.br/wp-content/uploads/2022/08/MicrosoftTeams-image-3.jpg";
+    }
+    if(image === 4) {
+        this.img_slide = "https://tems.com.br/wp-content/uploads/2022/08/encontro-cla╠a-18-.ml-7875.jpg";
+    }
+  }
+
 }
