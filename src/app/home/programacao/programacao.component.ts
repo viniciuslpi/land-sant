49c6214233bf4153618e92d36aa14013f6b2ea40
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-08-23 17:14:57 -0300

    settimeout

--- src/app/home/programacao/programacao.component.ts
+++ src/app/home/programacao/programacao.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-programacao',
@@ -6,12 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./programacao.component.scss']
 })
 
-export class ProgramacaoComponent {
+export class ProgramacaoComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.changeIMG();
+  }
 
   img_slide = "https://tems.com.br/wp-content/uploads/2022/08/1.jpg";
   img_slide_1 = "https://tems.com.br/wp-content/uploads/2022/08/1.jpg";
   img_slide_2 = "https://tems.com.br/wp-content/uploads/2022/08/2.jpg";
   img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/4.jpg";
+  cont = 1;
 
   changeSlide(image: Number) {
     
@@ -28,5 +33,21 @@ export class ProgramacaoComponent {
         this.img_slide = this.img_slide_1;
     }
   }
+
+  changeIMG(){
+    if(this.cont < 4) {
+      this.cont ++;
+    } else {
+      this.cont = 1;
+    }
+
+    setTimeout(() => {
+      this.img_slide = `https://tems.com.br/wp-content/uploads/2022/08/${this.cont}.jpg`;
+      this.changeIMG();
+    }, 5000);
+
+    
+  }
+
 
 }
