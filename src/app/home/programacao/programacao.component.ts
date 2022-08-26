2a83504c33b293d3e809df8bf30d889dbb3ae990
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-08-26 14:35:07 -0300

    new images

--- src/app/home/programacao/programacao.component.ts
+++ src/app/home/programacao/programacao.component.ts
@@ -12,7 +12,9 @@ export class ProgramacaoComponent implements OnInit {
   img_slide = "https://tems.com.br/wp-content/uploads/2022/08/1.jpg";
   img_slide_1 = "https://tems.com.br/wp-content/uploads/2022/08/1.jpg";
   img_slide_2 = "https://tems.com.br/wp-content/uploads/2022/08/2.jpg";
-  img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/4.jpg";
+  img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/3.jpg";
+  img_slide_4 = "https://tems.com.br/wp-content/uploads/2022/08/4.jpg";
+
   cont: any = 1;
 
   ative1: boolean = true;
@@ -55,18 +57,23 @@ export class ProgramacaoComponent implements OnInit {
       this.img_slide_1 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-1.jpg";
       this.img_slide_2 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-2.jpg";
       this.img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-3.jpg";
+      this.img_slide_4 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-4.jpg";
+
   }
   public verifyMediaMobile() {
       this.img_slide = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-1.jpg";
       this.img_slide_1 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-1.jpg";
       this.img_slide_2 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-2.jpg";
       this.img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-3.jpg";
+      this.img_slide_4 = "https://tems.com.br/wp-content/uploads/2022/08/mobile-slide-img-4.jpg";
+
   }
   public verifyMediaDesktop() {
       this.img_slide = "https://tems.com.br/wp-content/uploads/2022/08/1.jpg";
       this.img_slide_1 = "https://tems.com.br/wp-content/uploads/2022/08/1.jpg";
       this.img_slide_2 = "https://tems.com.br/wp-content/uploads/2022/08/2.jpg";
-      this.img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/4.jpg";
+      this.img_slide_3 = "https://tems.com.br/wp-content/uploads/2022/08/3.jpg";      
+      this.img_slide_4 = "https://tems.com.br/wp-content/uploads/2022/08/4.jpg";
   }
 
   changeSlide(option: any) {
@@ -99,7 +106,7 @@ export class ProgramacaoComponent implements OnInit {
       this.ative2 = false;
       this.ative3 = false;
       this.ative4 = true;
-      this.img_slide = this.img_slide_1;
+      this.img_slide = this.img_slide_4;
     }
   }
 
