8a947e4058526ed00f1b53ca722e98df53ef5031
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-08-26 11:43:25 -0300

    cleaning code

--- src/app/home/contato/contato.component.ts
+++ src/app/home/contato/contato.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.scss']
 })
-export class ContatoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ContatoComponent {
 
 }
